06e6d16a439c4c24e076d31410b5dba9d831e879
nokazn <41154684+nokazn@users.noreply.github.com>
2024-04-16 15:35:09 +0900

    docs: add example for test_each macro

--- src/utils/tests.rs
+++ src/utils/tests.rs
@@ -1,3 +1,27 @@
+/// ### Example
+///
+/// ```rust
+/// use crate::utils::tests::test_each;
+///
+/// fn add(a: i8, b: i8) -> i8 { a + b }
+///
+/// struct TestCase {
+///   input: (i8, i8),
+///   expected: i8,
+/// }
+///
+/// test_each!(
+///   test_add,
+///   |case: TestCase| {
+///     assert_eq!(
+///       add(case.input.0, case.input.1),
+///       case.expected
+///     );
+///   },
+///   "1" => TestCase { input: (1, 2), expected: 3, },
+///   "2" => TestCase { input: (4, 4), expected: 8, },
+/// );
+/// ```
 #[macro_export]
 macro_rules! test_each {
   ($name:ident, $fn:expr, $($suffix:expr => $case:expr,)*) => {
